7b3ff58df473e21944ac672dcaf8bde4875b94c1
ian otieno <ianotieno23@gmail.com>
2026-01-27 09:58:34 +0300

    update

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,6 @@
 import Job from '../models/JobModel.js';
 import {StatusCodes} from 'http-status-codes'
-
+import { NotFoundError } from '../errors/customErrors.js';
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -16,9 +16,7 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id)
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
