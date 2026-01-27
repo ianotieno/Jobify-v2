b8370bd2c0ad86c3f777289dfbfea094834f2339
ian otieno <ianotieno23@gmail.com>
2026-01-27 11:05:03 +0300

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 import jobRouter from './routes/jobRouter.js';
-
+import {body, validationResult} from 'express-validator';
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -18,6 +18,22 @@ if (process.env.NODE_ENV === 'development') {
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1/jobs', jobRouter);
+
+
+app.post('/api/v1/test',[body('name').notEmpty().withMessage('name is required')],
+(req,res,next)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((error) => error.msg);
+    return res.status(400).json({ errors: errorMessages });
+  }
+  next();
+},
+
+(req, res) => {
+  const {name} = req.body;
+  res.status(200).json({msg:`hello ${name}`});
+});
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'route does not exist' });
