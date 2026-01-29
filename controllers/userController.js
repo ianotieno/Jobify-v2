1c18f1e84a831b4a6741c2aa9faa6b42b09ad527
ian otieno <ianotieno23@gmail.com>
2026-01-29 11:06:41 +0300

    update push

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,5 +12,6 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
+  const updatedUser= await User.findOneAndUpdate(req.user.userId, req.body);   
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
