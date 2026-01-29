4648b3a264c29b0e1cc31bb8b3b47203b6f34720
ian otieno <ianotieno23@gmail.com>
2026-01-29 12:32:03 +0300

    update push

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,8 @@ import {
   updateUser,
 } from '../controllers/userController.js';
 
-import { validateUpdateUserInput,authorizePermissions } from '../middleware/validationMiddleware.js';
+import { validateUpdateUserInput} from '../middleware/validationMiddleware.js';
+import { authorizePermissions } from '../middleware/authMiddleware.js';
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats',authorizePermissions('admin'), getApplicationStats);
