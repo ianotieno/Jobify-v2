71bf9fe8a11af3a16b9db12cc6f17542813b1d1c
ian otieno <ianotieno23@gmail.com>
2026-01-29 10:44:33 +0300

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,10 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const getCurrentUser = async (req, res) => {
-  const user = await User.findOne({_id: req.user.userId});
-  res.status(StatusCodes.OK).json({ user });
-};
+    const user = await User.findOne({ _id: req.user.userId });
+    const userWithoutPassword = user.toJSON();
+    res.status(StatusCodes.OK).json({ user: userWithoutPassword });
+  };
 
 export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'application stats' });
