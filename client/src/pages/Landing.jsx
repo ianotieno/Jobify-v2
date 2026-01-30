06dbb34a3c065fcb86caa15f6d6070729d38afa2
ian otieno <ianotieno23@gmail.com>
2026-01-30 17:25:50 +0300

    update admin

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
 
-function Landing() {
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+
+const Landing = () => {
   return (
-    <h1>Landing</h1>
-  )
-}
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Stop the application chaos. Our Job Tracking App centralizes your
+            search, turning overwhelming spreadsheets into a streamlined command
+            center. Focus on the interview; we’ll handle the organization.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
