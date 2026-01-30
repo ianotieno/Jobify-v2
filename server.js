4f0ad50cf4990cd846eebaa10bcdb6d2d60cdf0c
ian otieno <ianotieno23@gmail.com>
2026-01-30 17:44:18 +0300

    update admin

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 import cookieParser from 'cookie-parser';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -23,6 +30,7 @@ app.use(express.json());
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ msg: 'test route' });
