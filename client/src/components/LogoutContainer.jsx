fe61dec5a606d2d5e9e0023dfaa54a2fbb0ddf71
ian otieno <ianotieno23@gmail.com>
2026-01-31 10:53:56 +0300

    update multer

--- client/src/components/LogoutContainer.jsx
+++ client/src/components/LogoutContainer.jsx
@@ -13,9 +13,13 @@ function LogoutContainer() {
       <button type='button' className='btn logout-btn'
       onClick={() => setShowLogout(!showLogout)}
       >
-      <FaUserCircle />
-      {user?.name}
-        <FaCaretDown />
+      {
+  user.avatar ? (
+    <img src={user.avatar} alt='avatar' className='img' />
+  ) : (
+    <FaUserCircle />
+  )
+}
       </button>
       <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
         <button type='button' className='dropdown-btn' onClick={logoutUser}>
--- server.js
+++ server.js
@@ -11,10 +11,14 @@ import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
 import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
+
 
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -32,8 +36,10 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 app.use('*', (req, res) => {
