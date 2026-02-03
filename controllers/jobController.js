fe88b596adbd1573a29d11a11e43dc1f2f9b6340
ian otieno <ianotieno23@gmail.com>
2026-02-03 12:20:36 +0300

    query

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -25,7 +25,18 @@ export const getAllJobs = async (req, res) => {
   if (jobType && jobType !== 'all') {
     queryObject.jobType = jobType;
   }
-  const jobs = await Job.find(queryObject);
+
+  const sortOptions = {
+    newest: '-createdAt',
+    oldest: 'createdAt',
+    'a-z': 'position',
+    'z-a': '-position',
+  };
+
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
+
+  const jobs = await Job.find(queryObject).sort(sortKey);
   res.status(StatusCodes.OK).json({ jobs });
 };
 
